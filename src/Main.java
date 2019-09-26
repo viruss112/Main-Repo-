e770b9c0616ca4d266fc8de1c7a53ed12b92ffef
Vlad Muresan <vlad.muresan96@yahoo.com>
2019-09-26 16:52:18 +0300

    even method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("fefewff");
+    }
+
+    public void evenNumbers(List<Integer> list){
+        for(Integer i:list){
+            if(i%2!=0)
+                System.out.println(i);
+        }
     }
 }
