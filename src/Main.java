fc5afd33fbd0dec965d3690c08b402826e2dc9eb
Vlad Muresan <vlad.muresan96@yahoo.com>
2019-09-26 16:50:03 +0300

    odd method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("fefewff");
+    }
+
+    public void printOdd(List<Integer> list){
+        for (Integer i:list){
+            if(i%2==0)
+                System.out.println(i);
+        }
     }
 }
