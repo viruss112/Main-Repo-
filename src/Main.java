2db0408b37ecc2a9fb4368bb9d1fab598b96fc47
Vlad Muresan <vlad.muresan96@yahoo.com>
2019-09-26 17:11:56 +0300

    master commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("fefewff");
         System.out.println("b2");
+        System.out.println("master");
     }
 
 
