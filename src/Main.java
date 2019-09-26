4b86e64e450f36f1cf23f122b7775871a6cc3421
Vlad Muresan <vlad.muresan96@yahoo.com>
2019-09-26 17:09:37 +0300

    sout(b2)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("fefewff");
-        System.out.println("b1");
+        System.out.println("b2");
     }
 
 
